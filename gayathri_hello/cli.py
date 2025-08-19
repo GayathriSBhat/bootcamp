6be670046bb4e2116c749c5a82bbb539a789f6fb
root <root@DESKTOP-TAEH51K.localdomain>
2025-08-19 17:24:47 +0000

    first

---
+++ gayathri_hello/cli.py
@@ -0,0 +1,11 @@
+# gayathri_hello/cli.py
+import sys
+
+def main():
+    name = "world"
+    if len(sys.argv) > 1:
+        name = sys.argv[1]
+    print(f"Hello, {name}!")
+
+if __name__ == "__main__":
+    main()
